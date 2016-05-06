30cc4e1f5016bc431934a42254cd760922cc1ebc
Rahul John Louis <rahul.louis@gmail.com>
2016-05-05 20:04:25 -0400

    AOP basics, Aspects, advice and configuring beans.xml to support AOP (aboutToTakePhoto advices before snap)

---
+++ AOP/src/com/caveofprogramming/spring/aop/Camera.java
@@ -0,0 +1,9 @@
+package com.caveofprogramming.spring.aop;
+
+public class Camera {
+
+	public void snap()
+	{
+		System.out.println("SNAP!");
+	}
+}
---
+++ AOP/src/com/caveofprogramming/spring/aop/Logger.java
@@ -0,0 +1,9 @@
+package com.caveofprogramming.spring.aop;
+
+public class Logger {
+
+	public void aboutToTakePhoto()
+	{
+		System.out.println("About to take photo.....");
+	}
+}
